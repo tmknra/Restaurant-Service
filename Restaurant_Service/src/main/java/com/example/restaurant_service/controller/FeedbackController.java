7194cc4556c9db1d3f51ae1d9ca29f0f6d154e4b
architect3232 <mr.moron56@gmail.com>
2022-08-23 13:17:45 +0500

    Refactored project

---
+++ Restaurant_Service/src/main/java/com/example/restaurant_service/controller/FeedbackController.java
@@ -0,0 +1,89 @@
+package com.example.restaurant_service.controller;
+
+import com.example.restaurant_service.dto.in.FeedbackInDto;
+import com.example.restaurant_service.dto.out.FeedbackOutDto;
+import com.example.restaurant_service.entity.Feedback;
+import com.example.restaurant_service.exception.entity.RestaurantNotFoundException;
+import com.example.restaurant_service.mapper.FeedbackMapper;
+import com.example.restaurant_service.service.FeedbackService;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/feedbacks")
+public class FeedbackController {
+
+    private final FeedbackService feedbackService;
+    private final FeedbackMapper feedbackMapper;
+
+    @Autowired
+    public FeedbackController(FeedbackService feedbackService, FeedbackMapper feedbackMapper) {
+        this.feedbackService = feedbackService;
+        this.feedbackMapper = feedbackMapper;
+    }
+
+    @GetMapping("/get/{id}")
+    public FeedbackOutDto getFeedbackById(@PathVariable Long id){
+        Feedback feedback = feedbackService.getFeedback(id);
+        return feedbackMapper.feedbackToFeedbackOutDto(feedback);
+    }
+
+    @GetMapping("/all/{restID}")
+    public Page<FeedbackOutDto> getFeedbacks(Pageable pageable, @PathVariable Long restID){
+        return feedbackService.getAllByRestaurantId(pageable, restID).map(feedbackMapper::feedbackToFeedbackOutDto);
+    }
+
+    @GetMapping("/get_rating/{id}")
+    public Double getAverageRatingByRestaurantID(@PathVariable Long id){
+       return feedbackService.getAverageRatingByRestaurantID(id);
+    }
+
+    @GetMapping("/{id}")
+    public String getFeedbackTextByID(@PathVariable Long id){
+        return feedbackService.getFeedbackTextByID(id);
+    }
+
+    @PostMapping("/new")
+    public FeedbackOutDto addFeedbackByRestaurantID(@RequestBody @Valid FeedbackInDto feedback) throws RestaurantNotFoundException {
+        return feedbackService.addNewFeedback(feedback.getRestaurantid(), feedback.getFeedback(), feedback.getRating());
+    }
+
+    @PostMapping("/change_feedback/{id}")
+    public Feedback changeFeedbackByID(@PathVariable Long id, @RequestBody @Valid FeedbackInDto feedback){
+        return feedbackService.changeFeedbackByID(id, feedback.getFeedback(), feedback.getRating());
+    }
+
+    @PostMapping("/delete")
+    public void deleteFeedbackById(@RequestBody JsonNode id){
+        feedbackService.deleteFeedbackById(id.get("id").asLong());
+    }
+
+    @PostMapping("/deleteAll/{restaurantId}")
+    public void deleteAllByRestaurantId(@PathVariable Long restaurantId){
+        feedbackService.deleteAllByRestaurantId(restaurantId);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException exception){
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach(
+                error -> {
+                    String fieldName = ((FieldError) error).getField();
+                    String errorMessage = error.getDefaultMessage();
+                    errors.put(fieldName, errorMessage);
+                }
+        );
+        return errors;
+    }
+}
---
+++ Restaurant_Service/src/main/java/com/example/restaurant_service/entity/Restaurant.java
@@ -0,0 +1,54 @@
+package com.example.restaurant_service.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "restaurants")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column
+    private String phone_number;
+
+    @Column
+    private String email_address;
+
+    @OneToMany(mappedBy = "restaurant",
+                cascade = CascadeType.PERSIST,
+                fetch = FetchType.LAZY)
+    @ToString.Exclude
+    private List<Feedback> feedbacks;
+
+    @Column
+    private LocalDate foundation_date;
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "(" +
+                "id = " + id + ", " +
+                "name = " + name + ", " +
+                "description = " + description + ", " +
+                "phone_number = " + phone_number + ", " +
+                "email_address = " + email_address + ", " +
+                "foundation_date = " + foundation_date + ")";
+    }
+}
