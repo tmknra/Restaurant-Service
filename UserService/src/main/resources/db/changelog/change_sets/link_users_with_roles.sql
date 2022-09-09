a36d720d83bc5c23d90ae0e6f8654ed3f27bc098
architect3232 <mr.moron56@gmail.com>
2022-09-09 15:28:28 +0500

    added security configuration (without role restriction)

--- UserService/src/main/resources/db/changelog/change_sets/link_users_with_roles.sql
+++ UserService/src/main/resources/db/changelog/change_sets/link_users_with_roles.sql
@@ -1,5 +1,5 @@
-create table if not exists user_role(
+create table if not exists roles_user(
     id serial primary key,
     user_id int not null references users(id),
-    role_id int not null references u_roles(id)
+    role_id int not null references roles(id)
 );
