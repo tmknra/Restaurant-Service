1b8ff2c4e34bc21e4f82153538c5379094389fb6
architect3232 <mr.moron56@gmail.com>
2022-10-11 11:37:41 +0500

    added RestaurantController mock tests

--- Restaurant_Service/src/test/java/com/example/restaurant_service/RestaurantServiceAppTests.java
+++ Restaurant_Service/src/test/java/com/example/restaurant_service/RestaurantServiceAppTests.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -20,8 +21,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = {
-        RestaurantServiceApp.class})
+        RestaurantServiceApp.class,
+        H2TestProfileJPAConfig.class})
 @ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:test-application.properties")
 public class RestaurantServiceAppTests {
 
     @Autowired
@@ -31,41 +34,40 @@ public class RestaurantServiceAppTests {
 
     @BeforeEach
     void contextLoads() throws Exception {
-        RestaurantInDto testRest = RestaurantInDto.builder()
-                .name("testRest")
-                .description("test")
-                .email_address("test@test.rr")
-                .phone_number("+79991122333")
-                .kitchenType(KitchenType.AMERICAN)
-                .build();
-        String json = objectMapper.writeValueAsString(testRest);
-        mockMvc.perform(post("/restaurants")
-                .contentType(MediaType.APPLICATION_JSON).content(json))
-                .andExpect(status().isOk());
-
-        RestaurantInDto testRest2 = RestaurantInDto.builder()
-                .name("testRest2")
-                .description("test2")
-                .email_address("test2@test.rr")
-                .phone_number("+79993322111")
-                .kitchenType(KitchenType.ITALIAN)
-                .build();
-        String json2 = objectMapper.writeValueAsString(testRest2);
-        mockMvc.perform(post("/restaurants")
-                .contentType(MediaType.APPLICATION_JSON).content(json2))
-                .andExpect(status().isOk());
-
-
-
-        FeedbackInDto testFeedback1 = FeedbackInDto.builder()
-                .restaurantId(1L)
-                .feedback("testFeedback")
-                .rating(3)
-                .build();
-        String json3 = objectMapper.writeValueAsString(testFeedback1);
-        mockMvc.perform(post("/restaurants")
-                        .contentType(MediaType.APPLICATION_JSON).content(json3))
-                .andExpect(status().isOk());
+        // RestaurantInDto testRest = RestaurantInDto.builder()
+        //         .name("testRest")
+        //         .description("test")
+        //         .email_address("test@test.rr")
+        //         .phone_number("+79991122333")
+        //         .kitchenType(KitchenType.AMERICAN)
+        //         .build();
+        // String json = objectMapper.writeValueAsString(testRest);
+        // mockMvc.perform(post("/restaurants")
+        //                 .contentType(MediaType.APPLICATION_JSON).content(json))
+        //         .andExpect(status().isOk());
+        //
+        // RestaurantInDto testRest2 = RestaurantInDto.builder()
+        //         .name("testRest2")
+        //         .description("test2")
+        //         .email_address("test2@test.rr")
+        //         .phone_number("+79993322111")
+        //         .kitchenType(KitchenType.ITALIAN)
+        //         .build();
+        // String json2 = objectMapper.writeValueAsString(testRest2);
+        // mockMvc.perform(post("/restaurants")
+        //                 .contentType(MediaType.APPLICATION_JSON).content(json2))
+        //         .andExpect(status().isOk());
+        //
+        //
+        // FeedbackInDto testFeedback1 = FeedbackInDto.builder()
+        //         .restaurantId(1L)
+        //         .feedback("testFeedback")
+        //         .rating(3)
+        //         .build();
+        // String json3 = objectMapper.writeValueAsString(testFeedback1);
+        // mockMvc.perform(post("/feedbacks")
+        //                 .contentType(MediaType.APPLICATION_JSON).content(json3))
+        //         .andExpect(status().isOk());
 
     }
 }
